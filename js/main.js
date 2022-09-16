961f757fc5aa15c4470556d709f183b2855aaa6a
KongGeon <66816095+KongGeon@users.noreply.github.com>
2022-09-16 15:21:29 +0900

    0916 ie확인 후 클라이언트 전달

--- js/main.js
+++ js/main.js
@@ -1,74 +1,68 @@
 // 모바일 체크 true면 모바일
-function Mobile(){
-	return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);}
-
-
-// 헤더 포커스 이벤트
- const headerListName = Array.prototype.slice.call(document.querySelectorAll('.header__menu > li > a'));
- const headerlastA = Array.prototype.slice.call(document.querySelectorAll('.header__step2:last-child'));
-
-
-if (Mobile()){// 모바일일 경우
-
-
-    for (let i = 0; i < headerListName.length; i++) {
-        const e = headerListName[i];
-        e.addEventListener('click',()=>{
-            if(e.parentNode.classList.contains('header-active')){
-                
-                headerListName.forEach(a => {
-                    a.parentNode.classList.remove('header-active')
-                });
-            }else{
-
-                headerListName.forEach(a => {
-                    a.parentNode.classList.remove('header-active')
-                });
-                e.parentNode.classList.add('header-active')
-            }
-        })
-
-    }
-}else{// 모바일 아닐 경우
-
-    for (let i = 0; i < headerlastA.length; i++) {
-        const e = headerlastA[i];
-        e.addEventListener('focusout',()=>{
-            e.parentNode.parentNode.classList.remove('header-active')    
-         })
-    }
+function Mobile() {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+  } // 헤더 포커스 이벤트
+  
+  
+  var headerListName = Array.prototype.slice.call(document.querySelectorAll('.header__menu > li > a'));
+  var headerlastA = Array.prototype.slice.call(document.querySelectorAll('.header__step2:last-child'));
+  
+  if (Mobile()) {// 모바일일 경우
+    var _loop = function _loop(i) {
+      var e = headerListName[i];
+      e.addEventListener('click', function () {
+        if (e.parentNode.classList.contains('header-active')) {
+          headerListName.forEach(function (a) {
+            a.parentNode.classList.remove('header-active');
+          });
+        } else {
+          headerListName.forEach(function (a) {
+            a.parentNode.classList.remove('header-active');
+          });
+          e.parentNode.classList.add('header-active');
+        }
+      });
+    };
     
-     for (let i = 0; i < headerListName.length; i++) {
-         const e = headerListName[i];
-         e.addEventListener('focusout',()=>{
-             headerListName.forEach(a => {
-                 a.parentNode.classList.remove('header-active')
-             });
-         })
-         e.addEventListener('focus',()=>{
-             headerListName.forEach(a => {
-                 a.parentNode.classList.remove('header-active')
-             });
-             e.parentNode.classList.add('header-active')
-         })
-     }
-     
-     const headerIn = document.querySelector('header');
-
-     headerIn.addEventListener('mouseleave',()=>{
-         headerListName.forEach(a => {
-             a.parentNode.classList.remove('header-active')
-         });
-     })
-
-
-}
-
-
- // 헤더 포커스 이벤트 끝
-
-// datepicker
- $.datepicker.setDefaults({
+    for (var i = 0; i < headerListName.length; i++) {
+      _loop(i);
+    }
+  } else {// 모바일 아닐 경우
+    var _loop2 = function _loop2(_i) {
+      var e = headerlastA[_i];
+      e.addEventListener('focusout', function () {
+        e.parentNode.parentNode.classList.remove('header-active');
+      });
+    };
+  
+    
+    for (var _i = 0; _i < headerlastA.length; _i++) {
+      _loop2(_i);
+    }
+  
+    var _loop3 = function _loop3(_i2) {
+      var e = headerListName[_i2];
+      e.addEventListener('focus', function () {
+        headerListName.forEach(function (a) {
+          a.parentNode.classList.remove('header-active');
+        });
+        e.parentNode.classList.add('header-active');
+      });
+      e.addEventListener('mouseleave', function () {
+        headerListName.forEach(function (a) {
+          a.parentNode.classList.remove('header-active');
+        });
+      });
+    };
+  
+    for (var _i2 = 0; _i2 < headerListName.length; _i2++) {
+      _loop3(_i2);
+    }
+  } // 헤더 포커스 이벤트 끝
+  // datepicker
+  
+  
+  $.datepicker.setDefaults({
     dateFormat: 'yy-mm-dd',
     prevText: '이전 달',
     nextText: '다음 달',
@@ -80,23 +74,16 @@ if (Mobile()){// 모바일일 경우
     showMonthAfterYear: true,
     yearSuffix: '년'
   });
-
-
-
-  function modalOn(modal){
+  
+  function modalOn(modal) {
     $('.' + modal).fadeIn();
-}
-
-$(function(){ 
+  }
+  
+  $(function () {
     //  모달
-    
-    $(".modal-close-btn").click(function(){
+    $(".modal-close-btn").click(function () {
       $(".modal").fadeOut();
-    });
- 
-    // datepicker
+    }); // datepicker
+  
     $('.date-nomal').datepicker();
   });
-
-
-
